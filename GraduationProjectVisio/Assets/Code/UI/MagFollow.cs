c268c882e46ba9c75c4fc7dc3e731afd2816c819
pjboer1 <boer.pim77@gmail.com>
2026-05-06 17:27:18 +0200

    - Made the magnifyer glass finally work correctly; - Changed the whole UI to World Space so i can have 2 cameras render it. - Made it work with a render texture so i dont have any offset or anchorpoints problems.

---
+++ GraduationProjectVisio/Assets/Code/UI/MagFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MagFollow : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera magnifierCamera;
+    public float distance = 2f;
+    public RectTransform magnifierRoot;
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(
+            new Vector3(mousePos.x, mousePos.y, distance)
+        );
+
+        magnifierCamera.transform.position = new Vector3(
+            worldPos.x,
+            worldPos.y,
+            magnifierCamera.transform.position.z
+        );
+
+        magnifierRoot.position = mousePos;
+    }
+}
